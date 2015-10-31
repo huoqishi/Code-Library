29d2c3bc0b3e02f219144a39beed58876fdee57f
huoqishi <wangchaohua110@outlook.com>
2015-10-31 16:51:08 +0800

    ok

--- 相关-js/isc-js判断手机浏览器还是PC.js
+++ 相关-js/isc-js判断手机浏览器还是PC.js
@@ -1,3 +1,7 @@
 if(navigator.userAgent.match(/(iPhone|iPod|iPod|Android|ios|Windows Phone|SymbianOS)/i)){
 location.replace('http://baidu.com');
 }
+
+if(navigator.userAgent.match(/(iPhone|iPod|iPod|Android|ios|Windows Phone|SymbianOS|Mobile)/i)){
+location.replace('http://baidu.com');
+}
